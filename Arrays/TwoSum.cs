a76ae263a1ef260bca8eefa136e8b306dd13ca25
Gokul <Gokul>
2025-08-02 17:02:31 +0530

    Initial commit

---
+++ Arrays/TwoSum.cs
@@ -0,0 +1,20 @@
+public static class TwoSum
+{
+    public static int[] BruteForce(int[] nums, int target)
+    {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿//Code to Execute TwoSum class BruteForce method
+int[] nums = { 2, 7, 11, 15 };
+int target = 9;
+int[] result = TwoSum.BruteForce(nums, target);
+Console.WriteLine($"Indices of the two numbers that add up to {target}: [{result[0]}, {result[1]}]");
